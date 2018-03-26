b999136e00e5d782e34beec05a6ea3944dcc198b
KenJoelTL <joellutumba@hotmail.com>
2018-03-26 19:05:16 -0400

    correction du lien vers l'api

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,13 +1,13 @@
-//var host = "https://tomcat.dept-info.crosemont.quebec"; //pour le live
+var host = "https://tomcat.dept-info.crosemont.quebec/Foodora-Comptabilite-API/comptabilite/transaction/"; //pour le live
 //var host = "http://localhost:8080"; //pour le test en localhost.
-var host = "http://localhost:8080/Foodora-Compatibilite-API/compatibilite/transaction/";
+//var host = "http://localhost:8080/Foodora-Compatibilite-API/compatibilite/transaction/";
 var myApp = angular.module('myApp', []);
 
 //Définition d'un controleur pour le module myApp
 myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
         //console.log("Coucou du controleur !");
 
-        //fonction pour reinitialiser le contact sélectionné
+        //fonction pour reinitialiser la transaction sélectionnée
         $scope.deselectionnerTransaction = function () {
             $scope.transaction = null;
         };
@@ -16,7 +16,7 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
 
         //fonction pour rafraichir la page.
         var rafraichir = function () {
-            //action lorsque requete get /contactlist
+            //action lorsque requete get /transaction
             $http.get(host).then(function (response) {
                 //console.log("J'ai reçu les données que j'ai demandées");
                 $scope.listeTransactions = response.data;
@@ -26,17 +26,17 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
 
         rafraichir(); // la fonction sera appelée lors du démmarage de l'app
 
-        //fonction pour ajouter un contact à la base de données
+        //fonction pour ajouter une transaction à la base de données
         $scope.ajouterTransaction = function () {
-            //console.log($scope.contact); //contact à ajouter à la bd
+            //console.log($scope.transaction); //transaction à ajouter à la bd
             $http.post(host, $scope.transaction).then(function (response) {
-                console.log(response.data); //contact ajouté à la bd
-                
+                console.log(response.data); //transaction ajoutée à la bd
+
                 rafraichir();
             });
         };
 
-        //fonction pour supprimer un contact de la bd
+        //fonction pour supprimer une transaction de la bd
         $scope.supprimerTransaction = function (id) {
             //console.log(id);
             $http.delete(host+id).then(function (response) {
@@ -44,7 +44,7 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
             });
         };
 
-        //fonction pour chercher un contact de la bd et le selectionner(mettre dans le formulaire)
+        //fonction pour chercher une transaction de la bd et le selectionner(mettre dans le formulaire)
         $scope.chercherTransaction = function (id) {
             //console.log(id);
             deselectionnerTransaction();
@@ -53,9 +53,9 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
             });
         };
 
-        //fonction pour modifier le contact sélectionné (dans le formulaire)
+        //fonction pour modifier le transaction sélectionnée (dans le formulaire)
         $scope.modifierTransaction = function () {
-            //console.log($scope.contact._id);
+            //console.log($scope.transaction.id);
             $http.put(host+$scope.transaction.id, $scope.transaction).then(function (response) {
                 //console.log(response.data);
                 rafraichir();
